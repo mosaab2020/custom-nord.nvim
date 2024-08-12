38dc112392529148b22f9898f6da49b5cdc4b3ee
mosaab2020 <mosaabomar@gmail.com>
2024-08-12 10:30:33 +0400

    fix error

--- lua/nord/theme.lua
+++ lua/nord/theme.lua
@@ -92,7 +92,7 @@ theme.loadEditor = function()
     MoreMsg = { fg = nord.nord4_gui },
     NonText = { fg = nord.nord1_gui },
     Pmenu = { fg = nord.nord4_gui, bg = nord.nord0_gui },
-    PmenuSel = { fg = nord.nord0_gui, bg = nord.nord10_gui_gui },
+    PmenuSel = { fg = nord.nord0_gui, bg = nord.nord10_gui },
     PmenuSbar = { fg = nord.nord4_gui, bg = nord.nord0_gui },
     PmenuThumb = { fg = nord.nord4_gui, bg = nord.nord0_gui },
     Question = { fg = nord.nord14_gui },
